04fd67f89df635cfa3ad901365565a74537646a5
Speedfan94 <43236732+Speedfan94@users.noreply.github.com>
2020-03-10 22:11:52 +0100

    update main.dart regarding multi language

--- test_flutter_layout/lib/main.dart
+++ test_flutter_layout/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'home_widget.dart';
 void main() => runApp(App());
 
@@ -9,8 +10,22 @@ class App extends StatelessWidget {
 
 
     return MaterialApp(
+      localizationsDelegates: [
+        // ...app-specific localizazion delegate[s] here
+        GlobalMaterialLocalizations.delegate, // not needed for WidgetsApp
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), //English)
+        const Locale('de'), //German
+        const Locale.fromSubtags(languageCode: 'de'),
+      ],
+
+
       title: 'My Flutter App',
       home: Home(),
     );
+    //print(Localizations.localeOf((context)));
   }
 }
